6487c02f3417a392ba3e7dd2f19177c6cc5f32dd
Olena Oleksandrova <ha240886kev@gmail.com>
2024-11-06 23:01:20 +0200

    form

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,17 @@
+const Contactform = () => {
+    return (
+        <div>
+            <label>
+                <span>Name</span>
+                <input type="text" ></input>
+            </label>
+            <label>
+                <span>Number</span>
+                <input type="text" ></input>
+            </label>
+            <button>Add contact</button>
+        </div>
+    )
+}
+
+export default Contactform
